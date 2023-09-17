adfc38a72e82e5ba78e0668685de0269f31a7c15
jfndi <143571177+jfndi@users.noreply.github.com>
2023-09-17 22:18:43 +0200

    Add Sensor project

---
+++ Chapter1/Sensor/Sensor.h
@@ -0,0 +1,20 @@
+#pragma once
+/*
+ * ## class Sensor in C.
+ */
+typedef struct Sensor Sensor;
+
+struct Sensor
+{
+	int filterFrequency;
+	int updateFrequency;
+	int value;
+};
+
+int Sensor_getFilterFrequency(const Sensor* const me);
+void Sensor_setFilterFrequency(Sensor* const me, int p_filterFrequency);
+int Sensor_getUpdateFrequency(const Sensor* const me);
+void Sensor_setUpdateFrequency(Sensor* const me, int p_updateFrequency);
+int Sensor_getValue(const Sensor* const me);
+Sensor* Sensor_Create(void);
+void Sensor_Destroy(Sensor* const me);
